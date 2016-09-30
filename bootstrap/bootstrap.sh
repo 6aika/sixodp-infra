4cad2e8a747e6cc2519cedc7f3c0e42e3c219c48
Ville Seppanen <ville.seppanen@gofore.com>
2016-09-30 12:14:36 +0300

    Port docker deployment from shell script. Add ssh key role

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+yum update -y
+yum install -y ansible git
+
+cd /root
+git clone https://github.com/6aika/sixodp-infra.git
+cd /root/sixodp-infra
+
+# TODO
+# aws s3 cp secrets...
+
+ansible-playbook -v -i localhost, deploy.yml > /root/deploy.log
